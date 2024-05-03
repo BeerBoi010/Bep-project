4a707bfbb7e647c6d0638ce52cac245643800c8b
FinnHoff <f.h.hoffmann@student.tudelft.nl>
2024-05-03 13:42:03 +0200

    means is edited but not finished

--- Mean code v1.py
+++ Mean code v1.py
@@ -1,11 +1,13 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 import sys
 
 #important variables:
-sample_size = 3
+window_size = 3
 
 
 
@@ -27,38 +29,33 @@ annotation5 = np.load("Data_tests/Annotated times/time_ranges_subject_5.npy", al
 annotation6 = np.load("Data_tests/Annotated times/time_ranges_subject_6.npy", allow_pickle=True)
 annotation7 = np.load("Data_tests/Annotated times/time_ranges_subject_7.npy", allow_pickle=True)
 
-# Define the label mapping dictionary
-label_mapping = {'N': 0, 'A': 1, 'B': 2, 'C': 3}
+data = [[1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15]]
+# Convert the array to a DataFrame
+df = pd.DataFrame(data)
+#acc2 = df['drinking_HealthySubject2_Test']
+print(df.head)
+
+# acc_dat = pd.DataFrame(acc)
+# acc_2 = acc_dat['drinking_HealthySubject2_Test']
+
+# print(acc_2)
 
 
+# Calculate rolling mean for each row with window size 3
+rolling_means = df.rolling(window=2,   min_periods=1).mean()
 
-#code to define the mean:
+#print(rolling_means)
+x_acceleration = acc['drinking_HealthySubject2_Test']['hand_IMU']
+x_accT = x_acceleration.T
 
-def mean(data):
-    mean_data= []
-    
-    
-    i = 0
-    # Initialize an empty list to store moving averages
-    moving_averages = []
-    
-    # Loop through the array to consider
-    # every window of size 3
-    while i < len(arr) - window_size + 1:
-    
-        # Store elements from i to i+window_size
-        # in list to get the current window
-        window = arr[i : i + window_size]
-    
-        # Calculate the average of current window
-        window_average = round(sum(window) / window_size, 2)
-        
-        # Store the average of current
-        # window in moving average list
-        moving_averages.append(window_average)
-        
-        # Shift window to right by one position
-        i += 1
-    
-    print(moving_averages)
-    return 
+dataset = pd.DataFrame(x_accT)
+roller = dataset.rolling(window=3,   min_periods=1).mean()
+
+print(roller[0])
+print(x_accT[0])
+
+# plt.figure()
+# plt.plot(x_accT[0])
+# plt.show()
