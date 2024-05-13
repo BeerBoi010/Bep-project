85c0db263bba12d2b6a094b37feeaf868a31aec4
nina-natasha <n.n.debruin@student.tudelft.nl>
2024-05-13 09:57:23 +0200

    ty

--- SVM - tryout/OVA.rbf.scaled.py
+++ SVM - tryout/OVA.rbf.scaled.py
@@ -21,7 +21,7 @@ sampling_window_mean = 3
 sampling_window_STD = 3
 sampling_window_slope = 3
 
-test_person = 4
+test_person = 3
 
 acc = np.load("Data_tests/ACC_signal.npy", allow_pickle=True).item()
 rot = np.load("Data_tests/Gyro_signal.npy", allow_pickle=True).item()
