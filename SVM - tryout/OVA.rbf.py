55009ee69e1fb09772087d8065b25a7fd7b3cae9
nina-natasha <n.n.debruin@student.tudelft.nl>
2024-05-13 09:48:15 +0200

    876

--- SVM - tryout/OVA.rbf.py
+++ SVM - tryout/OVA.rbf.py
@@ -21,7 +21,7 @@ sampling_window_mean = 3
 sampling_window_STD = 3
 sampling_window_slope = 3
 
-test_person = 6
+test_person = 4
 
 
 acc = np.load("Data_tests/ACC_signal.npy", allow_pickle=True).item()
