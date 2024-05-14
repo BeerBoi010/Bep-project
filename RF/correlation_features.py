d9b861947a47c48b778e6117cf8e0eee7476aeec
FinnHoff <f.h.hoffmann@student.tudelft.nl>
2024-05-14 12:31:25 +0200

    Co-authored-by: TjalleAalbers <TjalleAalbers@users.noreply.github.com> Set up correlation with a plot as well

--- RF/correlation_features.py
+++ RF/correlation_features.py
@@ -47,9 +47,11 @@ all_labels = labels_interpolation.expanded_matrices
 subjects = ['drinking_HealthySubject2_Test', 'drinking_HealthySubject3_Test', 'drinking_HealthySubject4_Test',   
         'drinking_HealthySubject5_Test', 'drinking_HealthySubject6_Test', 'drinking_HealthySubject7_Test']
 
+imu_locations = ['hand_IMU', 'lowerarm_IMU', 'upperarm_IMU', 'shoulder_IMU', 'sternum_IMU']
+
 # ########must be  into movements(not per measurement!)######################
 
-subjects.remove(f'drinking_HealthySubject{test_person}_Test')
+#subjects.remove(f'drinking_HealthySubject{test_person}_Test')
 subjects_train = subjects
 subjects_test = [f'drinking_HealthySubject{test_person}_Test']
 #print(subjects_test)
@@ -64,14 +66,14 @@ train_labels = all_labels
 #################################################################################################################
 ### Setting up the test and training sets with labels ###########################################################
 
-# X_train_RMS = RMS_V2.RMS_train(subjects_train, sampling_window_RMS, min_periods)
-# X_test_RMS = RMS_V2.RMS_test(subjects_test, sampling_window_RMS, min_periods)
+X_train_RMS = RMS_V2.RMS_train(subjects_train, sampling_window_RMS, min_periods)
+X_test_RMS = RMS_V2.RMS_test(subjects_test, sampling_window_RMS, min_periods)
 
-# X_train_Mean = Mean_V2.Mean_train(subjects_train, sampling_window_mean, min_periods)
-# X_test_Mean = Mean_V2.Mean_test(subjects_test, sampling_window_mean, min_periods)
+X_train_Mean = Mean_V2.Mean_train(subjects_train, sampling_window_mean, min_periods)
+X_test_Mean = Mean_V2.Mean_test(subjects_test, sampling_window_mean, min_periods)
 
-# X_train_Slope = Slope_V2.Slope_train(subjects_train, sampling_window_slope, min_periods)
-# X_test_Slope = Slope_V2.Slope_test(subjects_test, sampling_window_slope, min_periods)
+X_train_Slope = Slope_V2.Slope_train(subjects_train, sampling_window_slope, min_periods)
+X_test_Slope = Slope_V2.Slope_test(subjects_test, sampling_window_slope, min_periods)
 
 X_train_Max = Max_V2.Max_train(subjects_train, sampling_window_min_max, min_periods)
 X_test_Max = Max_V2.Max_test(subjects_test, sampling_window_min_max, min_periods)
@@ -79,11 +81,8 @@ X_test_Max = Max_V2.Max_test(subjects_test, sampling_window_min_max, min_periods
 X_train_Min = Min_V2.Min_train(subjects_train, sampling_window_min_max, min_periods)
 X_test_Min = Min_V2.Min_test(subjects_test, sampling_window_min_max, min_periods)
 
-# X_train_STD = Standard_Deviation.STD_train(subjects_train, sampling_window_STD, min_periods)
-# X_test_STD = Standard_Deviation.STD_test(subjects_test, sampling_window_STD, min_periods)
-
-# Y_train_labels = train_labels
-#Y_test_labels = test_labels
+X_train_STD = Standard_Deviation.STD_train(subjects_train, sampling_window_STD, min_periods)
+X_test_STD = Standard_Deviation.STD_test(subjects_test, sampling_window_STD, min_periods)
 
 
 # print(X_test_Slope["drinking_HealthySubject2_Test"]['hand_IMU']["acc_slope"][0])
@@ -97,32 +96,43 @@ corr = []
 #     correlation, _= pearsonr(x,y)
 #     corr.append(correlation)
 #     print(f'the correlation of column {number +1 } is:', {correlation })
-
+########################################################################################3
 
 # Dictionary to hold correlation lists for each subject
 correlations = {}
-# print(X_train_Min['drinking_HealthySubject3_Test']['hand_IMU']["acc_min"])
+
 # Iterate over each subject
 for subject in subjects:
     # Initialize the correlation list for the current subject
     corr = []
     
     # Get the number of rows (assuming all subjects have the same number of rows)
-    num_rows = 3#len(X_test_Max[subject]['hand_IMU']["acc_max"])
-    num_imu_sensors = 3
+    num_rows = 3
+
+    #number of imu sensors to iterate over
     
 
     # Iterate over the rows for the current subject
-    for number in range(num_rows):
-        for number in range(num_imu sensor):
-        x = X_train_Min[subject]['hand_IMU']["acc_min"][number]
-        y = X_train_Max[subject]['hand_IMU']["acc_max"][number]
-        correlation, _ = pearsonr(x, y)
-        corr.append(correlation)  # Append the correlation value to the list
-
+    for sensor in imu_locations:
+        
+        for number in range(num_rows):
+            x = X_train_Mean[subject][sensor]["acc_mean"][number]
+            y = X_train_RMS[subject][sensor]["acc_rms"][number]
+            correlation, _ = pearsonr(x, y)
+            corr.append(correlation)  # Append the correlation value to the list
+            
+            # print(f'The correlation for {subject} for the {sensor} in column {number +1} are {correlation}')
     # Store the correlation list in the dictionary
     correlations[subject] = corr
 
-# Print the correlation lists for each subject
-for subject, corr in correlations.items():
-    print(f'The correlations for {subject} are: {corr}')
+################ Plot that shows all of the correlation features between Min and max #########################
+plt.figure()
+plt.title('Correlation Values for Each Subject')
+plt.ylabel('Pearson Correlation Coefficient')
+for subject in subjects:    
+    plt.plot(correlations[subject], label =f'person {subject}')
+plt.legend()
+plt.show()
+# # Print the correlation lists for each subject
+# for subject, corr in correlations.items():
+#     print(f'The correlations for {subject} are: {corr}')
