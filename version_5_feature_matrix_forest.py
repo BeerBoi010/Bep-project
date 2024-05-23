4fcb5fd32f404a6c6a0de8ff65c307c94191953a
FinnHoff <f.h.hoffmann@student.tudelft.nl>
2024-05-23 12:59:43 +0200

    cleaned up

--- version_5_feature_matrix_forest.py
+++ version_5_feature_matrix_forest.py
@@ -321,7 +321,7 @@ print("Confusion Matrix:\n", conf_matrix)
 plt.figure(figsize=(8, 6))
 sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues',
             xticklabels=[label_mapping[key] for key in label_mapping.keys()],
-            yticklabels=[label_mapping[key] for key in label_mapping.keys()])
+            yticklabels=[label_mapping[key] for key in label_maAping.keys()])
 plt.xlabel('Predicted Labels')
 plt.ylabel('True Labels')
 plt.title(f'Confusion Matrix for {subject}')
