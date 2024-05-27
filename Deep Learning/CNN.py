998e06d83515f498ca4ffb5f6e861168a6c875b1
nina-natasha <n.n.debruin@student.tudelft.nl>
2024-05-27 12:43:52 +0200

    8764

--- Deep Learning/CNN.py
+++ Deep Learning/CNN.py
@@ -1,6 +1,42 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense
 
+import labels_interpolation
+
+
+train_amount = 5
+sampling_window = 3
+min_periods = 1
+test_amount = train_amount
+
+
+test_person = 7
+
+
+# Load data
+acc = np.load("Data_tests/ACC_signal.npy", allow_pickle=True).item()
+rot = np.load("Data_tests/Gyro_signal.npy", allow_pickle=True).item()
+all_labels = labels_interpolation.expanded_matrices
+
+# Prepare subjects and labels
+subjects = [f'drinking_HealthySubject{i+2}_Test' for i in range(6)]
+subjects.remove(f'drinking_HealthySubject{test_person}_Test')
+subjects_train = subjects
+subjects_test = [f'drinking_HealthySubject{test_person}_Test']
+
+test_labels = all_labels[test_person - 2]
+all_labels.pop(test_person - 2)
+train_labels = all_labels
+
+labels_train = [i[1] for item in train_labels for i in item]
+labels_test = [item[1] for item in test_labels]
+label_mapping = {'N': 0, 'A': 1, 'B': 2, 'C': 3}
+
+y_train = [label_mapping[label] for label in labels_train]
+y_test = [label_mapping[label] for label in labels_test]
+
+print(y_train)
